a6affe9ad6fed78a5286988fdc09c537c7a082b4
22BTRAD009 <132878422+Wane2412@users.noreply.github.com>
2024-05-08 10:36:36 +0530

    discussion-50 commit

---
+++ Day 10/discussion-50-solutions.sql
@@ -0,0 +1,22 @@
+-- 1.1
+-- Total Sales by Product Line
+SELECT p.productLine, SUM(od.quantityOrdered * od.priceEach) AS totalSales FROM orderdetails od JOIN products p ON od.productCode = p.productCode GROUP BY p.productLine;
+
+-- 1.2
+-- Total Quantity Sold by Product Line
+SELECT p.productLine, SUM(od.quantityOrdered) AS totalQuantitySold FROM orderdetails od JOIN products p ON od.productCode = p.productCode GROUP BY p.productLine;
+
+-- 2.1
+-- Average Sale Amount by Product Line with Filtering
+SELECT p.productLine, AVG(od.quantityOrdered * od.priceEach) AS averageSaleAmount FROM orderdetails od JOIN products p ON od.productCode = p.productCode GROUP BY p.productLine HAVING averageSaleAmount > 10000; -- Assume 10000 as the threshold
+
+-- 3.1
+-- Average Population and Total GDP by Continent
+SELECT c.Continent, AVG(c.Population) AS averagePopulation, SUM(c.GNP) AS totalGDP FROM country c GROUP BY c.Continent;
+
+-- 3.2
+-- Countries with Multiple Official Languages
+SELECT co.Code, co.Name, COUNT(*) AS numberOfOfficialLanguages FROM countrylanguage cl JOIN country co ON cl.CountryCode = co.Code WHERE cl.IsOfficial = 'T' GROUP BY co.Code, co.Name HAVING numberOfOfficialLanguages > 3; -- Assume 3 as the count threshold
+
+-- 4.1
+-- 
